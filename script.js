171c1ee46f3fd82345f723ab098c3d022beb474a
Anna <136835388+Freyanna010@users.noreply.github.com>
2024-01-27 16:25:14 +0300

    27/01

---
+++ script.js
@@ -0,0 +1,52 @@
+const words = [
+  {
+    original: "Wassup",
+    translation: "здарова",
+  },
+  {
+    original: "Bay",
+    translation: "покеда",
+  },
+  {
+    original: "Programmer",
+    translation: "программист",
+  },
+];
+
+const notificationMessages = {
+  ansver: {
+    right: "Хорошо!",
+    wrong: "Мимо(",
+  },
+  result: {
+    Succes: "Молодец!",
+    UnSucces: "Увы, Вы дурилка(",
+  },
+};
+
+const settings = {
+  correctAnswerMinPercent: 50,
+};
+
+const result = {
+  correctAnswerCount: 0,
+};
+
+for (let i = 0; i < words.length; i++) {
+  let userAnsver = prompt(`Переведите слово: ${words[i].original}`, "");
+  let correctUserAnsver = userAnsver.toLowerCase();
+  if (correctUserAnsver === words[i].translation) {
+    document.write(notificationMessages.ansver.right, "<br/>");
+    result.correctAnswerCount++;
+  } else {
+    document.write(notificationMessages.ansver.wrong, "<br/>");
+  }
+}
+
+const usserCorrectAnswerPersent =
+  (result.correctAnswerCount / words.length) * 100;
+if (usserCorrectAnswerPersent > settings.correctAnswerMinPercent) {
+  alert(notificationMessages.result.Succes);
+} else {
+  alert(notificationMessages.result.UnSucces);
+}
